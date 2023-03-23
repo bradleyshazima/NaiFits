2e0a0007b0e1c3557a40078ece7528e1c1f55f13
amos.ahindukha <amos@bposeats.com>
2023-03-23 04:12:15 +0300

    Commit

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    autoplay: true,
+    autoplayTimeout: 1000,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+//   ----------------------------------------------------------- OWL CAROUSEL INITIALIZATION ------------------------------------------------------
+  
+
+$('.loop').owlCarousel({
+    center: true,
+    items:2,
+    loop:true,
+    margin:10,
+    responsive:{
+        600:{
+            items:4
+        }
+    }
+});
